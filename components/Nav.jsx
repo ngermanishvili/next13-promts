eb63107d882bcbd48088ab1438cfe61223211220
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-08 18:02:28 +0400

    refactor

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,46 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { SignIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const isUserLoggedIn = true;
+  
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="promtopia logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promtopia</p>
+
+        {/* Desktop navigation */} 
+        <div className="sm:flex hidden">
+          {isUserLoggedIn ? (
+            <div className="flex gap-3 md:gap-5">
+              <Link href="/create-prompt" className="black_btn">
+                Create Post
+              </Link>
+              <button
+                type="button"
+                onClick={signOut}
+                className="outline-btn"
+              ></button>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+      </Link>
+    </nav>
+  );
 };
 
 export default Nav;
