13647d5284828e94d31f34e6e9a56e9716483c04
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-05-11 18:48:43 +0400

    Done: Nav Component with mobile friendly response design

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,44 +1,130 @@
 "use client";
 
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import { useState, useEffect } from "react";
-import { SignIn, signOut, useSession, getProviders } from "next-auth/react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
   const isUserLoggedIn = true;
-  
-
+  const [providers, setProviders] = useState(null);
+  const [toggleDropDown, settoggleDropDown] = useState(false);
+  // async function is a promise that will return a value in the future
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      // setProviders is for the useState hook to set the value of providers to the response from getProviders function
+      setProviders(response);
+    };
+    setProviders();
+  });
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image
           src="/assets/images/logo.svg"
-          alt="promtopia logo"
+          alt="logo"
           width={30}
           height={30}
           className="object-contain"
         />
-        <p className="logo_text">Promtopia</p>
-
-        {/* Desktop navigation */} 
-        <div className="sm:flex hidden">
-          {isUserLoggedIn ? (
-            <div className="flex gap-3 md:gap-5">
-              <Link href="/create-prompt" className="black_btn">
-                Create Post
-              </Link>
-              <button
-                type="button"
-                onClick={signOut}
-                className="outline-btn"
-              ></button>
-            </div>
-          ) : (
-            <></>
-          )}
-        </div>
+        <p className="logo_text">Promptopia</p>
       </Link>
+      {/* Dektop Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          // In here i'm using the providers object to map through the providers and display the sign in button for each provider in the providers object that is returned from the getProviders function in the useEffect hook.
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provder.id)}
+                  className="black"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative ">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+              // Its a bad practice to  change state in react like settoggleDropDown(!toggleDropDown).
+              onClick={() => settoggleDropDown((prev) => !prev)}
+            />
+            {toggleDropDown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => settoggleDropDown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create"
+                  className="dropdown_link"
+                  onClick={() => settoggleDropDown(false)}
+                >
+                  Create Promt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    settoggleDropDown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provder.id)}
+                  className="black"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
   );
 };
